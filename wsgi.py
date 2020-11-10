d0bea4922c13f60b0a8e038d8282e244435fa473
Gerardo Corsan <gerardo.corsan@gmail.com>
2020-11-10 15:17:53 -0600

    Hello there

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "Hello world!\n", 200, { 'Content-Type': 'text/plain' }
+    return "Hello there from Openshift!\n", 200, { 'Content-Type': 'text/plain' }
 
 if __name__ == '__main__':
     application.run(debug = True)
